8d40c41ab7c189e19a347f1ba07cd5e10a48eb27
piyush <piyushsingh0992@gmail.com>
2020-12-23 23:15:16 +0530

    first-edit

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
+
+// defining variables
 var keys=document.querySelectorAll('.key');
+var allAudioTags=document.querySelectorAll("audio");
+var background=document.querySelector('body');
 
 
-
+// defining functions
 function clickHandler(event){
-    console.log(event.id);
+    var audioClass=event.target.className;
+    
+    event.target.classList.add("playing");
+    
+    allAudioTags.forEach(eachtag=>{
+        eachtag.pause();
+    })
+
+    var x=document.querySelector(`audio[class=${audioClass}]`);
+
+    x.currentTime=0;
+    x.play();
+
+    setTimeout(function(){
+        event.target.classList.remove("playing");
+    }, 1300)
+    // background.style.backgroundImage = `url('resources/${audioClass}.jpg')`;
+
+
 }
 
+//adding eventlistner's
 keys.forEach(key=>{
     key.addEventListener('click',clickHandler);
 })
