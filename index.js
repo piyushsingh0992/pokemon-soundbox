cd3a13ae8a381926d146fe772fc01ac39fef4e98
piyush <piyushsingh0992@gmail.com>
2020-12-23 23:50:32 +0530

    final-edit

--- index.js
+++ index.js
@@ -5,30 +5,32 @@ var allAudioTags=document.querySelectorAll("audio");
 var background=document.querySelector('body');
 
 
+
 // defining functions
 function clickHandler(event){
+    // taking class of the image on which user clicked
     var audioClass=event.target.className;
-    
-    event.target.classList.add("playing");
-    
+
+    // pausing all the previous audio so that only one audio will play at a time
     allAudioTags.forEach(eachtag=>{
         eachtag.pause();
     })
 
-    var x=document.querySelector(`audio[class=${audioClass}]`);
+    //playing the desired audio for the button user clicked on
+    var audiotag=document.querySelector(`audio[class=${audioClass}]`);
+    audiotag.play();
 
-    x.currentTime=0;
-    x.play();
+    // adding playing class to enlarge the image 
+    event.target.classList.add("playing");
 
+    //removing playing class after some time to bring image to normal size
     setTimeout(function(){
         event.target.classList.remove("playing");
     }, 1300)
-    // background.style.backgroundImage = `url('resources/${audioClass}.jpg')`;
-
-
 }
 
-//adding eventlistner's
+
+//adding eventlistner's to all the key's
 keys.forEach(key=>{
     key.addEventListener('click',clickHandler);
 })
